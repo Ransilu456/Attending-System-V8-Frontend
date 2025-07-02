dcfbe92ff6e1933422ac6a47f4448377913eb0c8
Ransilu456 <samarasekarakeshan@gmail.com>
2025-07-02 15:20:45 +0530

    Update StudentsPage.jsx

--- frontend/src/pages/main/StudentsPage.jsx
+++ frontend/src/pages/main/StudentsPage.jsx
@@ -239,7 +239,7 @@ const StudentsPage = () => {
           <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
               <div className="overflow-hidden shadow-md rounded-lg border border-gray-200 dark:border-slate-700">
-                <table className="min-w-full divide-y divide-gray-300 dark:divide-slate-700">
+                <table className="min-w-full table-fixed divide-y divide-gray-300 dark:divide-slate-700">
                   <thead className="bg-gray-50 dark:bg-slate-800">
                     <tr>
                       <th
@@ -280,7 +280,7 @@ const StudentsPage = () => {
                       </th>
                     </tr>
                   </thead>
-                  <tbody className="divide-y divide-gray-200 dark:divide-slate-700 bg-white dark:bg-slate-800">
+                  <tbody className="divide-y divide-gray-200 dark:divide-slate-700 bg-white dark:bg-slate-800 max-h-[500px] overflow-y-auto w-full">
                     {loading ? (
                       <tr>
                         <td
